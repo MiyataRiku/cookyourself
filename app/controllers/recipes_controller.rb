2769b3ef45d20bad7df37490a02c33f0d803a4a9
Miyata Riku <takaya.mio.1015@gmail.com>
2025-06-20 01:12:31 +0900

    カロリー検索のカラム名のタイプミスを修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,7 @@ class RecipesController < ApplicationController
     if params[:search].present?
       search_term = "%#{params[:search]}%"
       # kcal カラムをテキスト型にキャストしてから LIKE 検索を行う
-    @recipes = Recipe.where("name LIKE ? OR material LIKE ? OR CAST(kcalGe AS TEXT) LIKE ? OR CAST(time AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE ?",
+    @recipes = Recipe.where("name LIKE ? OR material LIKE ? OR CAST(kcal AS TEXT) LIKE ? OR CAST(time AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE ?
                             search_term, search_term, search_term, search_term, search_term, search_term)
       # または、Rails 5 以降で推奨される記法
       # @recipes = Recipe.where("name ILIKE :search OR material ILIKE :search OR CAST(kcal AS TEXT) ILIKE :search", search: search_term)

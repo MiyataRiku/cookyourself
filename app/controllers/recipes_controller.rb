28ee38eed6b3ce4b2c7b6196da039d8cc5d58196
Miyata Riku <takaya.mio.1015@gmail.com>
2025-06-20 00:57:28 +0900

    timeとpriceを追加した

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,8 @@ class RecipesController < ApplicationController
     if params[:search].present?
       search_term = "%#{params[:search]}%"
       # kcal カラムをテキスト型にキャストしてから LIKE 検索を行う
-      @recipes = Recipe.where("name LIKE ? OR material LIKE ? OR CAST(kcalGe AS TEXT) LIKE? OR CAST(time AS TEXT) LIKE ? OR CAST(money AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE?", search_term, search_term, search_term)
+    @recipes = Recipe.where("name LIKE ? OR material LIKE ? OR CAST(kcalGe AS TEXT) LIKE ? OR CAST(time AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE ? OR CAST(price AS TEXT) LIKE ?",
+                            search_term, search_term, search_term, search_term, search_term, search_term)
       # または、Rails 5 以降で推奨される記法
       # @recipes = Recipe.where("name ILIKE :search OR material ILIKE :search OR CAST(kcal AS TEXT) ILIKE :search", search: search_term)
       # ILIKE は大文字・小文字を区別しない検索です。

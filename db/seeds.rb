da346b413677c8ec1c5502af42aae6e81ae6fc6b
Miyata Riku <takaya.mio.1015@gmail.com>
2025-06-20 02:29:23 +0900

    カロリー検索のカラム名のタイプミスを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,16 @@
+tags_to_create = [
+  { name: 'ごはんもの' },
+  { name: '肉料理' },
+  { name: '魚料理'},
+  { name: 'サラダ'},
+  { name: 'スープ'},
+  { name: '麺'},
+  { name: 'パン'},
+  { name: 'お菓子'},
+  { name: 'シーズン'},
+  { name: '一品料理'}
+]
 
-Tag.create([
-    { name: 'ごはんもの' },
-    { name: '肉料理' },
-    { name: '魚料理'},
-    { name: 'サラダ'},
-    { name: 'スープ'},
-    { name: '麺'},
-    { name: 'パン'},
-    { name: 'お菓子'},
-    { name: 'シーズン'},
-    { name: '一品料理'}
-    ])
-
+tags_to_create.each do |tag_data|
+  Tag.find_or_create_by(name: tag_data[:name])
+end
